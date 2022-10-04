a6c581cc29d1bc768bab5e3c82a87129de024cf5
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-10-04 13:09:58 +0200

    Play a single round
    unfinished and unedited

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let options = ["rock", "paper", "scissors"]
+let options = ["r", "p", "s"]
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * 3)];
 }
 
-const playerSelection = "Rock".toLowerCase()
+const playerSelection = "R".toLowerCase()
 const computerSelection = getComputerChoice()
 
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "r":
+            switch (computerSelection) {
+                case "r":
+                    return "It's a tie!";
+                    break;
+                case "p":
+                    return "You lose! Paper beats Rock!";
+                    break;
+                case "s":
+                    return "You win! Rock beats Scissors!";
+                    break;   
+            }
+            break;
+        case "p":
+            switch (computerSelection) {
+                case "r":
+                    return "It's a tie!";
+                    break;
+                case "p":
+                    return "You lose! Paper beats Rock!";
+                    break;
+                case "s":
+                    return "You win! Rock beats Scissors!";
+                    break;   
+            }
+            break;
+    }
