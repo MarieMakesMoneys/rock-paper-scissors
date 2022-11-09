95cd906892ffb917a8118be7806b8d861fbbb5af
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-11-09 18:07:29 +0100

    Revise display of outcome and stop game after 5win Add Refresh button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = document.querySelector('.user-score');
+let computerScore = document.querySelector('.computer-score');
 
 const btnRock = document.querySelector('.rock');
 const btnPaper = document.querySelector('.paper');
@@ -32,84 +32,59 @@ btnScissors.addEventListener('click', () => {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        const p = document.createElement('p');
-        p.textContent = `It's a tie! You both picked ${playerSelection}! The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        outcome.textContent = `It's a tie! You both picked ${playerSelection}!`
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        computerScore++;
-        const p = document.createElement('p');
-        p.textContent = `Computer wins! Paper beats Rock!  The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        computerScore.textContent = Number(computerScore.textContent) + 1;
+        outcome.textContent = `Computer wins! Paper beats Rock!`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        playerScore++;
-        const p = document.createElement('p');
-        p.textContent = `You win! Rock beats Scissors! The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        playerScore.textContent = Number(computerScore.textContent) + 1;
+        outcome.textContent = `You win! Rock beats Scissors!`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore++;
-        const p = document.createElement('p');
-        p.textContent = `Computer wins! Scissors beat Paper! The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        computerScore.textContent = Number(computerScore.textContent) + 1;
+        outcome.textContent = `Computer wins! Scissors beat Paper!`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        const p = document.createElement('p');
-        p.textContent = `You win! Paper beats Rock! The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        playerScore.textContent = Number(computerScore.textContent) + 1;
+        outcome.textContent = `You win! Paper beats Rock!`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore++;
-        const p = document.createElement('p');
-        p.textContent = `Computer wins! Rock beats Scissors! The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        computerScore.textContent = Number(computerScore.textContent) + 1;
+        outcome.textContent = `Computer wins! Rock beats Scissors!`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        const p = document.createElement('p');
-        p.textContent = `You win! Scissors beat Paper! The score is ${playerScore} for you and ${computerScore} for the computer.`;
-        outcome.appendChild(p);
+        playerScore.textContent = Number(computerScore.textContent) + 1;
+        outcome.textContent = `You win! Scissors beat Paper!`;
     }
 };
+
+
 function checkForWinner(playerScore, computerScore) {
-    if (playerScore === 5) {
-        const h2 = document.createElement('h2');
-        h2.textContent = "You've won the game, congrats !!!";
-        outcome.appendChild(h2);
+    if (Number(playerScore.textContent) === 5) {
+        outcome.textContent = "You've won the game, congratulations !!! Click the button to play another round!";
     }
-    
-    if (computerScore === 5) {
-        const h2 = document.createElement('h2');
-        h2.textContent = "Uh oh, the machine has won...";
-        outcome.appendChild(h2);
+
+    if (Number(computerScore.textContent) === 5) {
+        outcome.textContent = "The computer has won... Click the button to play another round!";
+    }
+
+    if (Number(computerScore.textContent) >= 5 || Number(playerScore.textContent >= 5)) {
+        btnPaper.removeEventListener('click', () => {
+            const computerSelection = getComputerChoice();
+            const playerSelection = 'paper';
+            playRound(playerSelection, computerSelection);
+            checkForWinner(playerScore, computerScore);
+        })
+        btnRock.removeEventListener('click', () => {
+            const computerSelection = getComputerChoice();
+            const playerSelection = 'rock';
+            playRound(playerSelection, computerSelection);
+            checkForWinner(playerScore, computerScore);
+        })
+        btnScissors.removeEventListener('click', () => {
+            const computerSelection = getComputerChoice();
+            const playerSelection = 'scissors';
+            playRound(playerSelection, computerSelection);
+            checkForWinner(playerScore, computerScore);
+        })
     }
 }
 
-
-
-
-function game(roundNum) {
-    let playerWin = 0;
-    let computerWin = 0;
-
-    for (let i = 0; i < roundNum; i++) {
-        let playerSelection = buttonClicked()
-        let computerSelection = getComputerChoice();
-        
-        console.log(playRound(playerSelection, computerSelection));
-        
-        if (playerSelection === "rock" && computerSelection === "scissors" ||
-        playerSelection === "paper" && computerSelection === "rock" ||
-        playerSelection === "scissors" && computerSelection === "paper" ) {
-            playerWin = playerWin + 1
-        } else if (playerSelection === "rock" && computerSelection === "paper" ||
-        playerSelection === "paper" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "rock") {
-            computerWin = computerWin + 1
-        }
-    }
-    
-    if (playerWin > computerWin) {
-        console.log("You won! What a game!")
-    } else if (playerWin < computerWin) {
-        console.log("The machine has won, we are doomed.")
-    } else {
-        console.log("It's a full tie, play another game ?")
-    }
-};
+const playAgain = document.querySelector('.play-again')
+playAgain.addEventListener('click', () => window.location.reload());
